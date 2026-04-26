964f3c52bb5c9457aac7c155254b9ceb55012bd6
IvoP <basedorpolo.2003@gmail.com>
2026-04-26 20:03:22 -0300

    Level 3 jugable fin

--- Assets/Escenas/animation_player.gd
+++ Assets/Escenas/animation_player.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,10 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-func _return() -> void:
-	pass
-
-func _forced_teleport(a:Vector2) -> void:
 	pass
--- Assets/Escenas/plataforma_cueva.gd
+++ Assets/Escenas/plataforma_cueva.gd
@@ -1,19 +1,11 @@
 extends AnimatableBody2D
 
-var speed = 100
-var inicio : Vector2
-var dir = Vector2(0,-1)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	inicio = position
-	print(position-inicio)
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += speed*delta*dir
-	print(dir)
-	print(position)
-	print(position-inicio)
-	if(abs(position-inicio) >=  Vector2(0,50)):
-		dir= Vector2(0,1)
-	if(position-inicio >= Vector2(0,50)):
-		dir= Vector2(0,-1)
+	pass
+
+func _Activate() -> void:
+	$AnimationPlayer.current_animation="movfin"
