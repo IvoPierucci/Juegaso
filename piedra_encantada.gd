cad45aa125bfd83b1a1089ed037c90f6b3781b52
IvoP <basedorpolo.2003@gmail.com>
2026-04-26 20:31:31 -0300

    Juego terminable

--- piedra_encantada.gd
+++ piedra_encantada.gd
@@ -4,9 +4,12 @@ extends Area2D
 @onready var m1 = $Med1
 @onready var m2 = $Med2
 @onready var m3 = $Med3
+@onready var plata = $plataformaCueva
+var nom : String
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
+	nom=get_tree().current_scene.name
 	pass # Replace with function body.
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,4 +27,10 @@ func _on_body_entered(body: Node2D) -> void:
 		m2.visible=true
 	if(medialunas>=3):
 		m3.visible=true
+		if(nom == "Level1"):
+			get_tree().change_scene_to_file("res://Assets/Escenas/level_2.tscn")
+		if(nom == "level2"):
+			get_tree().change_scene_to_file("res://Assets/Escenas/level_3.tscn")
+		if(nom == "level_3"):
+			plata._Activate()
 		
